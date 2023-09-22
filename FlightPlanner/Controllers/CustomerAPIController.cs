64023fc3f3983284a05c2712f4e11e1979bb983e
JV-33 <virsnitisjanis@icloud.com>
2023-09-22 23:54:00 +0300

    HMmm 04

---
+++ FlightPlanner/Controllers/CustomerAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace FlightPlanner.Controllers
+{
+    [AllowAnonymous]
+    [Route("api")]
+    [ApiController]
+    public class CustomerAPIController : ControllerBase
+    {
+        private readonly FlightStorage _storage;
+
+        public CustomerAPIController(FlightStorage storage)
+        {
+            _storage = storage;
+        }
+
+        [Route("airports")]
+        [HttpGet]
+        public IActionResult SearchAirports(string search)
+        {
+            if (string.IsNullOrEmpty(search))
+                return BadRequest("Search term is missing or empty");
+
+            var airports = _storage.GetUniqueAirports(search);
+            if (airports == null || !airports.Any())
+                return NotFound("No airports found");
+
+            return Ok(airports);
+        }
+    }
+}
+
+
---
+++ FlightPlanner/Storage/FlightStorage.cs
@@ -0,0 +1,88 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Controllers;
+
+
+namespace FlightPlanner.Storage
+{
+    public class FlightStorage
+    {
+        private static List<Flight> _flightStorage = new List<Flight>();
+        private static int _id = 0;
+
+        public void AddFlight(Flight flight)
+        {
+            flight.ID = _id++;
+            _flightStorage.Add(flight);
+
+        }
+
+        public void Clear()
+        {
+            _flightStorage.Clear();
+        }
+
+        public bool FlightExists(int id)
+        {
+            return _flightStorage.Any(f => f.ID == id);
+        }
+
+
+        public Flight GetExistingFlight(Flight flight)
+        {
+            return _flightStorage.FirstOrDefault(f =>
+                f.From.Country == flight.From.Country &&
+                f.From.City == flight.From.City &&
+                f.From.AirportCode == flight.From.AirportCode &&
+                f.To.Country == flight.To.Country &&
+                f.To.City == flight.To.City &&
+                f.To.AirportCode == flight.To.AirportCode &&
+                f.DepartureTime == flight.DepartureTime
+            );
+        }
+
+        public Flight? GetFlight(int id)
+        {
+            if (_flightStorage.Any(flight => flight.ID == id))
+            {
+                return _flightStorage.First(flight => flight.ID == id);
+            }
+            return null;
+        }
+
+        public bool DeleteFlight(int id)
+        {
+            if (!FlightExists(id))
+            {
+                return false;
+            }
+
+            var flightToRemove = _flightStorage.First(flight => flight.ID == id);
+            _flightStorage.Remove(flightToRemove);
+            return true;
+        }
+
+
+
+
+
+        public List<Airport> GetUniqueAirports(string search)
+        {
+            var searchLower = search.ToLowerInvariant();
+
+            var allAirports = _flightStorage.SelectMany(f => new[] { f.From, f.To })
+                                            .Where(a => a.AirportCode.ToLowerInvariant().Contains(searchLower) ||
+                                                        a.City.ToLowerInvariant().Contains(searchLower) ||
+                                                        a.Country.ToLowerInvariant().Contains(searchLower))
+                                            .ToList();
+
+            var uniqueAirports = allAirports
+                .GroupBy(a => a.AirportCode)
+                .Select(g => g.First())
+                .ToList();
+
+            return uniqueAirports;
+        }
+
+    }
+}
+
